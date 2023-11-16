8b3d2e47e01b1eb0498fb370eb987cf1402ef95b
SpeedupGuru <paul@speedup.guru>
2023-11-16 23:11:56 +0400

    homepage update

---
+++ _sunil_homepage_update/assets/homepage_product.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    new Swiper('.swiper-container', {
+		centeredSlides: false,
+        nextButton: false,
+        prevButton: false,
+        slidesPerView: 4.7,
+        paginationClickable: true,
+        spaceBetween: 15,
+       
+    });
+});
