ec6384024bc4f4ca6b88cc6bdfa30d6f1786e74c
SpeedupGuru <paul@speedup.guru>
2023-11-27 16:53:06 +0300

    add homepage

---
+++ _sunil_homepage/assets/_sunil_component-bf-bundleCycleSlider.js
@@ -0,0 +1,25 @@
+//BF Bundle Banner slider
+var swiper = new Swiper(".bundleCycleSlider", {
+  initialSlide: 3,
+  effect: "cards",
+  // loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+    delay: 4000,
+  },
+  cardsEffect: {
+    perSlideOffset: 8, // Space between cards in px
+    perSlideRotate: 0, // Rotation of cards in degrees
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+});
