a62918b4ff58cd381b5a236c9f818044ad018772
speedup-guru <paul@speedup.guru>
2023-12-22 16:12:37 +0400

    add ledMask page

---
+++ _sunil_led-mask/assets/js/_sunil_skinSlider.js
@@ -0,0 +1,14 @@
+//skincare slider
+var swiper = new Swiper(".skin_slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+
+});
