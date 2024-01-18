c5a1509be23f51c8d8ecec2556378eb15764b4e1
speedup-guru <paul@speedup.guru>
2024-01-18 18:16:31 +0400

    add _sunil_mi-landing-new.html

---
+++ _sunil_q-urify-replacement/assets/js/_sunil_component-seeSlider3.js
@@ -0,0 +1,29 @@
+//see slider
+var swiper = new Swiper(".see_slider3", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: false,
+  /*autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },*/
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '"><span class="pg_counter counter_title">STEP</span><span class="pg_counter counter_dot"></span>' + '<span class="pg_counter  counter_block">0' + (index + 1) + "</span></span>";
+    },
+  },
+  breakpoints: {
+    556: {
+      slidesPerView: 1,
+
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
